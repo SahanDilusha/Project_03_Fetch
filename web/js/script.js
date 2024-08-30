bbc4c79100f4a04795f8fcf2ca977294ddc08e09
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-30 11:01:32 +0530

    postRequest()

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,23 @@
-function getReuest(){
-    fetch("X?name=Sahan");
+function getReuest() {
+    fetch("X?name=Sahan")
+            .then(
+                    response => {
+                        console.log("Done");
+                        
+                    }
+            ).then(
+            text => {
+                console.log(text);
+            }
+    ).catch(
+            error => {
+                console.log(error);
+            }
+    );
+}
+
+function postRequest() {
+    
+    fetch("");
+    
 }
