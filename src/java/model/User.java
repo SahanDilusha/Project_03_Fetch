e6015b0d2e14a93bda279d4f5410a49dda8348cf
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-30 11:19:25 +0530

    create User bean

---
+++ src/java/model/User.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    public User() {
+
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+}
